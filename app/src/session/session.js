276adf9040329d7b8f20b5b2136aff426ab23722
Matteo Lecca <matteolecca@MacBookdiMatteo.home-life.hub>
2020-10-13 10:24:57 +0200

    MySQL session added

--- app/src/session/session.js
+++ app/src/session/session.js
@@ -1,4 +1,14 @@
 const session = require('express-session')
+var MySQLStore = require('express-mysql-session')(session);
+
+var options = {
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b3cc0996cbc46f',
+    password: '22721ae4',
+    database: 'heroku_ddaec690967f493'
+};
+ 
+var sessionDB = new MySQLStore(options);
 
 exports.session = session({
     secret: 'mysecret',
@@ -8,5 +18,6 @@ exports.session = session({
         //Store cookie one year
         maxAge: 24 * 365 * 60 * 60 * 1000,
         httpOnly: true
-    }
+    },
+    store : sessionDB
 })
